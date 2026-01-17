116a5b7b95688bc52f8c2cf9313fe437ecc13bec
CEK32 <contreras.camilo32@gmail.com>
2026-01-17 19:49:23 -0300

    Empty load

---
+++ src/test/java/com/cek32/ai/SummaryAI/SummaryAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cek32.ai.SummaryAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SummaryAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
